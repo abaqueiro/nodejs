920654bfdf6a7c32bc4ab76bae77872f45d9e30b
Alfonso Baqueiro Bernal <abaqueiro@gmail.com>
2024-06-10 17:43:16 -0600

    Code reordering for using handlers and routes.
    	modified:   pirple-mc/e02-rest-api/app.js

--- pirple-mc/e02-rest-api/app.js
+++ pirple-mc/e02-rest-api/app.js
@@ -5,6 +5,7 @@ const LISTEN_PORT = 8080
 const http = require('http')
 const url = require('url')
 const StringDecoderLib = require('string_decoder')
+const { chown } = require('fs')
 
 // server scope variables
 let request_count = 0
@@ -20,11 +21,12 @@ const server = http.createServer( function( request, response ){
 
 	//let url_trimmed_path = url_path.replace(/\/+/g,'/')
 	let url_trimmed_path = url_path.replace(/^\/+|\/+$/g,'')
-	console.log(`url_trimmed_path: ${url_trimmed_path}`)
+	if ( url_trimmed_path == '' ) {
+		url_trimmed_path = '/'
+	}
 
 	// Get query string
-	let request_params = parsed_url.query
-	console.log("Request parameters: ", request_params)
+	let request_query_params = parsed_url.query
 
 	// Get payload if any
 	let decoder = new StringDecoderLib.StringDecoder('utf-8')
@@ -36,65 +38,109 @@ const server = http.createServer( function( request, response ){
 	request.on('end',function(){
 		buffer.push( decoder.end() )
 
-		// Send response
-		response.write( `<html>
-			<head>
-			<style>
-			* {
-				box-sizing: border-box;
-			}
-			html, body {
-				margin: 0;
-				padding: 0;
-				font-size: 16pt;
-			}
-			header {
-				background-color: DarkBlue;
-				color: white;
-				text-align: center;
-				padding: 16px;
-				font-size: 24pt;
-				font-weight: bold;
-			}
-			footer {
-				display: block;
-				border: 1px solid black;
-				text-align: center;
-				/*position: absolute;
-				bottom: 0px;*/
-				width: 100%;
-				background-color: DarkSlateBlue;
-				color: white;
-			}
-			</style>
-			</head>
-			<body>
-			<header>Nodejs at your service</header>
-			` )
-					response.write( "\n<h2>Request Method</h2>\n" )
-					response.write( request.method )
-					response.write( "\n<h2>Request Url</h2>\n" )
-					response.write( request.url )
-					response.write( "\n<h2>Request Headers</h2>\n" )
-					response.write( "\n<xmp>" )
-					for( prop in request.headers ){
-						response.write(`${prop} : ${request.headers[prop]}\n`)
-					}
-					response.write( "</xmp>\n" )
-					response.write( "\n<h2>Payload</h2>\n" )
-					response.write( "\n<xmp>" )
-					response.write( buffer.join('') )
-					response.write( "</xmp>\n" )
-					response.write( `<footer>
-			<h3>Page requests count: ${request_count}</h3>
-			</footer>
-			</body>
-			</html>` )
-					response.end( "\n" )
+		// chose request handler
+		request_handler = typeof( route_index[ url_trimmed_path ] ) == 'undefined' ? handlers.not_found : route_index[ url_trimmed_path ]
 
+		// prepare request handler data
+		let request_params = {
+			url: request.url
+			, headers: request.headers
+			, method: request.method
+			, path: url_trimmed_path
+			, request_query_params: request_query_params
+			, body: buffer.join('')
+		}
+
+		// call the handler
+		request_handler( request_params, function( response_params ){
+			let status_code = typeof(response_params.status_code) == 'number' ? response_params.status_code : 500
+			response.writeHead( status_code )
+			
+			let response_body = typeof(response_params.body) == 'string' ? response_params.body : ''
+			response.write( response_body )
+			response.end( "\n" )
+		} ) 
+		
 	})
 } )
 
 server.listen( LISTEN_PORT, function(){
 	console.log(`[INFO] Server is listening on port ${LISTEN_PORT} ... `)
 },  )
+
+
+// HANDLERS
+const handlers = {}
+handlers.index = function( request_params, response_handler ){
+	let response_params = {}
+
+	let buffer = []
+	buffer.push( `<html>
+		<head>
+		<style>
+		* {
+			box-sizing: border-box;
+		}
+		html, body { 
+			margin: 0;
+			padding: 0;
+			font-size: 16pt;
+		}
+		header {
+			background-color: DarkBlue;
+			color: white;
+			text-align: center;
+			padding: 16px;
+			font-size: 24pt;
+			font-weight: bold;
+		}
+		footer {
+			display: block;
+			border: 1px solid black;
+			text-align: center;
+			/*position: absolute;
+			bottom: 0px;*/
+			width: 100%;
+			background-color: DarkSlateBlue;
+			color: white;
+		}
+		</style>
+		</head>
+		<body>
+		<header>Nodejs at your service</header>
+		` )
+		buffer.push( "\n<h2>Request Method</h2>\n" )
+		buffer.push( request_params.method )
+		buffer.push( "\n<h2>Request Url</h2>\n" )
+		buffer.push( request_params.url )
+		buffer.push( "\n<h2>Request Headers</h2>\n" )
+		buffer.push( "\n<xmp>" )
+		for( prop in request_params.headers ){
+			buffer.push(`${prop}: ${request_params.headers[prop]}\n`)
+		}
+		buffer.push( "</xmp>\n" )
+		buffer.push( "\n<h2>Payload</h2>\n" )
+		buffer.push( "\n<xmp>" )
+		buffer.push( request_params.body )
+		buffer.push( "</xmp>\n" )
+		buffer.push( `<footer>
+		<h3>Page requests count: ${request_count}</h3>
+		</footer>
+		</body>
+		</html>` )
+
+	response_params.status_code = 200
+	response_params.body = buffer.join('')
+	response_handler( response_params )
+}
+handlers.not_found = function( request_params, response_handler ){
+	let response_params = {}
+	response_params.status_code = 404
+	response_params.body = '{}'
+	response_handler( response_params )
+}
+
+// REQUEST ROUTING
+const route_index = {
+	'/': handlers.index
+}
